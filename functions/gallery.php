5e94e0ccb3edf22d38f39268bf8d8b264b95ea32
arazgholami <contact@arazgholami.com>
2018-01-10 22:05:29 +0330

    Fixed functions conflict with wordpress

--- functions/gallery.php
+++ functions/gallery.php
@@ -32,7 +32,3 @@ function neo_gallery_shortcode($attr) {
     $output .= "</div>";
     return $output;
 }
-
-?>
-
-
--- functions/lastupdate.php
+++ functions/lastupdate.php
@@ -4,5 +4,3 @@ function lastUpdate() {
     return get_the_time('l, M jS, Y');
 }
 add_shortcode('lastupdate', 'lastUpdate');
-
-?>
